faa96f941b51ee8405dc95cca5410e4fe847fc1d
Pavlo15 <pavlo.mysak1@gmail.com>
2018-02-21 17:42:42 +0300

    Test
    TestMessage

--- StateOne/StateOne/MainWindow.xaml.cs
+++ StateOne/StateOne/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace StateOne
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Привіт)");
+        }
     }
 }
